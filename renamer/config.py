14358a9fd219cf09bf70061e9126b3bf34a0eb2f
Merashshsh <112298844+Merashshsh@users.noreply.github.com>
2022-08-30 16:56:52 +0530

    Add files via upload

--- renamer/config.py
+++ renamer/config.py
@@ -4,6 +4,7 @@ class Config:
     ACTIVE_DOWNLOADS = {}
     API_ID = int(os.environ.get("API_ID", 12345))
     API_HASH = os.environ.get("API_HASH")
+    PAID_BOT = os.environ.get("PAID_BOT", "YES")
     AUTH_USERS =  [int(i) for i in os.environ.get("AUTH_USERS", "").split(" ")]
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     DATABASE_URL = os.environ.get("DATABASE_URL", "")
