9a2a452e7aab6e3b521d569652d51cb22f317d52
Merashshsh <112298844+Merashshsh@users.noreply.github.com>
2022-08-30 17:11:05 +0530

    Update config.py

--- renamer/config.py
+++ renamer/config.py
@@ -2,12 +2,12 @@ import os
 
 class Config:
     ACTIVE_DOWNLOADS = {}
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
+    API_ID = int(os.environ.get("API_ID", '6795023'))
+    API_HASH = os.environ.get("API_HASH", '48eb04ae416967495ba9930f87d4f4da')
     PAID_BOT = os.environ.get("PAID_BOT", "YES")
-    AUTH_USERS =  [int(i) for i in os.environ.get("AUTH_USERS", "").split(" ")]
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
+    AUTH_USERS =  [int(i) for i in os.environ.get("AUTH_USERS", "1805398747").split(" ")]
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5028427286:AAGggDShZZwX7lnfkeaH9i0FalwBBkh2cGU")
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://dkbotz:786@cluster0.pczec.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     DB_CHANNEL_ID = -1001602376426
     RESTART_TIME = []
