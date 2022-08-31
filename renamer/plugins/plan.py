429d1ee31d0e2ebbf5785b08736ff334b20844aa
Merashshsh <112298844+Merashshsh@users.noreply.github.com>
2022-08-31 18:24:55 +0530

    Update plan.py

--- renamer/plugins/plan.py
+++ renamer/plugins/plan.py
@@ -65,7 +65,7 @@ async def help(c, m):
         else:
             current_date = datetime.datetime.now()
             await m.reply_text(
-                text="**Your Plan Deatails**\n\n**User Id:** `{m.from_user.id}`\n\n**User Name:** @{m.from_user.username}\n\nPlan : `Free`\n\n**Plan Validity:** `Lifetime`\n\nDate :- {current_date}\n\n__**Plan 1**__\n\n`49/M\n\nAll Features Of This Bot`\n\n__**Plan 2**__\n\n`130 For 3 Month\n\nAll Features Of This Bot`\n\n__**Plan 2**__\n\n`500 For 1 Year\n\nAll Features Of This Bot`\n\n||**Need More Plans Contact To Our Developer :- @DKBOTZHELP**||",
+                text=f"**Your Plan Deatails**\n\n**User Id:** `{m.from_user.id}`\n\n**User Name:** @{m.from_user.username}\n\nPlan : `Free`\n\n**Plan Validity:** `Lifetime`\n\nDate :- {current_date}\n\n__**Plan 1**__\n\n`49/M\n\nAll Features Of This Bot`\n\n__**Plan 2**__\n\n`130 For 3 Month\n\nAll Features Of This Bot`\n\n__**Plan 2**__\n\n`500 For 1 Year\n\nAll Features Of This Bot`\n\n||**Need More Plans Contact To Our Developer :- @DKBOTZHELP**||",
                 disable_web_page_preview=True,
                 quote=True,
             )
@@ -80,8 +80,7 @@ async def help(c, m):
         integer_paid_duration = int(paid_duration)
         will_expire = paid_on + datetime.timedelta(days=integer_paid_duration)
         await m.reply_text(
-            text="**Your Plan Deatails**\n\n**User Id:** `{m.from_user.id}`\n\n**User Name:** @{m.from_user.username}\n\nPlan Type : `Paid`\n\n**Plan Validity:** `{paid_duration}` Days\n\n**Plan Buy On** : `{paid_on}`\n\n**Plan Discription** : `{paid_reason}`\n\nDate :- `{current_date}`\n\n👋 Your Paid Plan Has Expired On {will_expire}\n\n__**Plan 1**__\n\n`49/M\n\nAll Features Of This Bot`\n\n__**Plan 2**__\n\n`130 For 3 Month\n\nAll Features Of This Bot`\n\n__**Plan 2**__\n\n`500 For 1 Year\n\nAll Features Of This Bot`\n\n||**Need More Plans Contact To Our Developer :- @DKBOTZHELP**||",
-            reply_markup=SUB_BUTTONS,
+            text=f"**Your Plan Deatails**\n\n**User Id:** `{m.from_user.id}`\n\n**User Name:** @{m.from_user.username}\n\nPlan Type : `Paid`\n\n**Plan Validity:** `{paid_duration}` Days\n\n**Plan Buy On** : `{paid_on}`\n\n**Plan Discription** : `{paid_reason}`\n\nDate :- `{current_date}`\n\n👋 Your Paid Plan Has Expired On {will_expire}\n\n__**Plan 1**__\n\n`49/M\n\nAll Features Of This Bot`\n\n__**Plan 2**__\n\n`130 For 3 Month\n\nAll Features Of This Bot`\n\n__**Plan 2**__\n\n`500 For 1 Year\n\nAll Features Of This Bot`\n\n||**Need More Plans Contact To Our Developer :- @DKBOTZHELP**||",
             disable_web_page_preview=True,
             quote=True,
         )
