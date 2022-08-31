8561fa4f68e113b64bb17c9a7219b4b557090fe3
Merashshsh <112298844+Merashshsh@users.noreply.github.com>
2022-08-31 07:40:27 +0530

    Update premium_check.py

--- renamer/tools/premium_check.py
+++ renamer/tools/premium_check.py
@@ -121,7 +121,6 @@ async def premium_check(c, m, sent):
                 pass
 
         else:
-            await send.delete()
             await m.reply_text(
                 text="Only paid users can use me. For more information Click on **Instructions** Buttons",
                 reply_markup=SUB_BUTTONS,
