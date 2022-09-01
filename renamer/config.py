8491da1c4a06b94300491a617a5902bc58ac3253
Merashshsh <112298844+Merashshsh@users.noreply.github.com>
2022-09-01 16:45:20 +0530

    Update config.py

--- renamer/config.py
+++ renamer/config.py
@@ -5,6 +5,7 @@ class Config:
     API_ID = int(os.environ.get("API_ID", '6795023'))
     API_HASH = os.environ.get("API_HASH", '48eb04ae416967495ba9930f87d4f4da')
     PAID_BOT = os.environ.get("PAID_BOT", "YES")
+    SESSION_STRING = os.environ.get("SESSION_STRING")
     AUTH_USERS =  [int(i) for i in os.environ.get("AUTH_USERS", "1805398747").split(" ")]
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "5028427286:AAGggDShZZwX7lnfkeaH9i0FalwBBkh2cGU")
     DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://dkbotz:786@cluster0.pczec.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
